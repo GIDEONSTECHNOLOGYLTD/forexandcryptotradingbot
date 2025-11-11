cc14ea54eefb2ca6883a889dce9193b9a4dc890d
GIDEONSTECHNOLOGYLTD <ceo@gideonstechnology.com>
2025-11-11 21:01:23 +0000

    fix: Add app.config.js to force correct bundle ID, remove invalid field from eas.json

---
+++ mobile-app/app.config.js
@@ -0,0 +1,62 @@
+export default {
+  expo: {
+    name: "Trading Bot Pro",
+    slug: "trading-bot-pro",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#667eea"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.gtechldt.tradingbot",
+      buildNumber: "1",
+      infoPlist: {
+        CFBundleIconName: "AppIcon",
+        NSCameraUsageDescription: "This app uses the camera to scan QR codes for payments",
+        NSPhotoLibraryUsageDescription: "This app needs access to your photo library to save charts"
+      },
+      config: {
+        usesNonExemptEncryption: false
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#667eea"
+      },
+      package: "com.gtechldt.tradingbot",
+      versionCode: 1,
+      permissions: [
+        "NOTIFICATIONS",
+        "INTERNET"
+      ]
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "49b56a0e-70ba-4d62-abe4-5928343098e1"
+      }
+    },
+    plugins: [
+      [
+        "expo-notifications",
+        {
+          icon: "./assets/notification-icon.png",
+          color: "#667eea"
+        }
+      ],
+      "expo-font"
+    ],
+    owner: "gtechldt"
+  }
+};
